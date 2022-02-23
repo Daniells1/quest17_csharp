c7eb50bc8128c750cf0513024a9e237d5b083118
Daniel <danielleitesilva15@gmail.com>
2022-02-23 00:16:15 -0300

    Adicionar arquivos de projeto.

---
+++ quest17_csharp/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Crie um programa para ler 2 números e imprimir qual o menor entre eles ou se eles 
+são iguais.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quest17_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            if (number1 > number2)
+            {
+                Console.WriteLine(" O maior número é: {0}", number1);
+            }
+            else if (number1 < number2) {
+                Console.WriteLine("O maior número é: {0]", number2);
+            }
+            else
+            {
+                Console.WriteLine("Os números são iguais");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
